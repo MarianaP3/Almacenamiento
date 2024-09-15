3fdbbc9e79f2d8265813c21f4a558b5f3a5472d2
Mariana Pérez <marianasps003@gmail.com>
2024-09-14 20:02:35 -0600

    Formulario Proveedor

---
+++ ProyectoAlmacenamiento/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ProyectoAlmacenamiento
+{
+    partial class Proveedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textNombre_Proveedor = new System.Windows.Forms.TextBox();
+            this.textCorreo_Proveedor = new System.Windows.Forms.TextBox();
+            this.textTelefono_Proveedor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textDomicilio_Proveedor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.botonClickInsertar = new System.Windows.Forms.Button();
+            this.botonClickEliminar = new System.Windows.Forms.Button();
+            this.botonClickModificar = new System.Windows.Forms.Button();
+            this.TablaProveedor = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.TablaProveedor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textNombre_Proveedor
+            // 
+            this.textNombre_Proveedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre_Proveedor.Location = new System.Drawing.Point(314, 71);
+            this.textNombre_Proveedor.Name = "textNombre_Proveedor";
+            this.textNombre_Proveedor.Size = new System.Drawing.Size(338, 30);
+            this.textNombre_Proveedor.TabIndex = 0;
+            // 
+            // textCorreo_Proveedor
+            // 
+            this.textCorreo_Proveedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCorreo_Proveedor.Location = new System.Drawing.Point(314, 122);
+            this.textCorreo_Proveedor.Name = "textCorreo_Proveedor";
+            this.textCorreo_Proveedor.Size = new System.Drawing.Size(338, 30);
+            this.textCorreo_Proveedor.TabIndex = 1;
+            // 
+            // textTelefono_Proveedor
+            // 
+            this.textTelefono_Proveedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTelefono_Proveedor.Location = new System.Drawing.Point(314, 174);
+            this.textTelefono_Proveedor.Name = "textTelefono_Proveedor";
+            this.textTelefono_Proveedor.Size = new System.Drawing.Size(338, 30);
+            this.textTelefono_Proveedor.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(139, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Teléfono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(139, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Correo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(139, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Domicilio fiscal";
+            // 
+            // textDomicilio_Proveedor
+            // 
+            this.textDomicilio_Proveedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDomicilio_Proveedor.Location = new System.Drawing.Point(314, 223);
+            this.textDomicilio_Proveedor.Name = "textDomicilio_Proveedor";
+            this.textDomicilio_Proveedor.Size = new System.Drawing.Size(338, 30);
+            this.textDomicilio_Proveedor.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(292, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "PROVEEDOR";
+            // 
+            // botonClickInsertar
+            // 
+            this.botonClickInsertar.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonClickInsertar.Location = new System.Drawing.Point(144, 279);
+            this.botonClickInsertar.Name = "botonClickInsertar";
+            this.botonClickInsertar.Size = new System.Drawing.Size(116, 31);
+            this.botonClickInsertar.TabIndex = 9;
+            this.botonClickInsertar.Text = "Insertar";
+            this.botonClickInsertar.UseVisualStyleBackColor = true;
+            this.botonClickInsertar.Click += new System.EventHandler(this.botonClickInsertar_Click);
+            // 
+            // botonClickEliminar
+            // 
+            this.botonClickEliminar.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonClickEliminar.Location = new System.Drawing.Point(519, 279);
+            this.botonClickEliminar.Name = "botonClickEliminar";
+            this.botonClickEliminar.Size = new System.Drawing.Size(116, 31);
+            this.botonClickEliminar.TabIndex = 10;
+            this.botonClickEliminar.Text = "Eliminar";
+            this.botonClickEliminar.UseVisualStyleBackColor = true;
+            this.botonClickEliminar.Click += new System.EventHandler(this.botonClickEliminar_Click);
+            // 
+            // botonClickModificar
+            // 
+            this.botonClickModificar.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonClickModificar.Location = new System.Drawing.Point(334, 279);
+            this.botonClickModificar.Name = "botonClickModificar";
+            this.botonClickModificar.Size = new System.Drawing.Size(116, 31);
+            this.botonClickModificar.TabIndex = 11;
+            this.botonClickModificar.Text = "Modificar";
+            this.botonClickModificar.UseVisualStyleBackColor = true;
+            this.botonClickModificar.Click += new System.EventHandler(this.botonClickModificar_Click);
+            // 
+            // TablaProveedor
+            // 
+            this.TablaProveedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TablaProveedor.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.TablaProveedor.Location = new System.Drawing.Point(95, 341);
+            this.TablaProveedor.Name = "TablaProveedor";
+            this.TablaProveedor.RowHeadersWidth = 51;
+            this.TablaProveedor.RowTemplate.Height = 24;
+            this.TablaProveedor.Size = new System.Drawing.Size(617, 149);
+            this.TablaProveedor.TabIndex = 12;
+            this.TablaProveedor.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TablaProveedor_CellContentClick_1);
+            // 
+            // Proveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(800, 521);
+            this.Controls.Add(this.TablaProveedor);
+            this.Controls.Add(this.botonClickModificar);
+            this.Controls.Add(this.botonClickEliminar);
+            this.Controls.Add(this.botonClickInsertar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textDomicilio_Proveedor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textTelefono_Proveedor);
+            this.Controls.Add(this.textCorreo_Proveedor);
+            this.Controls.Add(this.textNombre_Proveedor);
+            this.Name = "Proveedor";
+            this.Text = "Proveedor";
+            this.Load += new System.EventHandler(this.Proveedor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TablaProveedor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textNombre_Proveedor;
+        private System.Windows.Forms.TextBox textCorreo_Proveedor;
+        private System.Windows.Forms.TextBox textTelefono_Proveedor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textDomicilio_Proveedor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button botonClickInsertar;
+        private System.Windows.Forms.Button botonClickEliminar;
+        private System.Windows.Forms.Button botonClickModificar;
+        private System.Windows.Forms.DataGridView TablaProveedor;
+    }
+}
+
---
+++ ProyectoAlmacenamiento/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace ProyectoAlmacenamiento
+{
+
+    public partial class Proveedor : Form
+    {
+        SqlConnection nuevaConexion = new SqlConnection("Server = MARIANAPC\\SQLEXPRESS; " +
+            "DATABASE = Almacenamiento; integrated security=true");
+        string IdProveedor = "";
+        string nombre_proveedor = "";
+        string telefono_proveedor = "";
+        string correo_proveedor = "";
+        string domicilio_fiscal_proveedor = "";
+        public Proveedor()
+        {
+            InitializeComponent();
+            ConsultaDatos();
+        }
+
+        public void ConsultaDatos()
+        {
+            nuevaConexion.Open();
+            TablaProveedor.DataSource = null;
+            String selectInfo = "SELECT * FROM almacen.proveedor";
+            SqlCommand scm = new SqlCommand(selectInfo, nuevaConexion);
+            SqlDataReader lector = scm.ExecuteReader();
+
+            if (TablaProveedor.Columns.Count == 0)
+            {
+                TablaProveedor.Columns.Add("IdProveedor", "ID");
+                TablaProveedor.Columns.Add("Nombre_proveedor", "Nombre");
+                TablaProveedor.Columns.Add("Telefono_proveedor", "Teléfono");
+                TablaProveedor.Columns.Add("Correo_proveedor", "Correo");
+                TablaProveedor.Columns.Add("Domicilio_fiscal_proveedor", "Domicilio");
+            }
+
+            TablaProveedor.Rows.Clear();
+
+            int i = 0;
+            while (lector.Read())
+            {
+                TablaProveedor.Rows.Add();
+                TablaProveedor.Rows[i].Cells[0].Value = lector["IdProveedor"].ToString();
+                TablaProveedor.Rows[i].Cells[1].Value = lector["NombreProveedor"].ToString();
+                TablaProveedor.Rows[i].Cells[2].Value = lector["TelefonoProveedor"].ToString();
+                TablaProveedor.Rows[i].Cells[3].Value = lector["CorreoProveedor"].ToString();
+                TablaProveedor.Rows[i].Cells[4].Value = lector["DomicilioFiscal"].ToString();
+                i++;
+            }
+
+            lector.Close();
+            nuevaConexion.Close();
+        }
+
+        public void InsertaDato()
+        {
+            nombre_proveedor = textNombre_Proveedor.Text;
+            telefono_proveedor = textTelefono_Proveedor.Text;
+            correo_proveedor = textCorreo_Proveedor.Text;
+            domicilio_fiscal_proveedor = textDomicilio_Proveedor.Text;
+
+
+            nuevaConexion.Open();
+
+            string insertInfo = "INSERT INTO almacen.Proveedor(NombreProveedor, " +
+                "TelefonoProveedor, CorreoProveedor, DomicilioFiscal) VALUES ('" + nombre_proveedor + "'," +
+         "'" + telefono_proveedor + "','" + correo_proveedor + "' , '" + domicilio_fiscal_proveedor + "' )";
+
+            SqlCommand cm = new SqlCommand(insertInfo, nuevaConexion);
+            cm.ExecuteNonQuery();
+
+            nombre_proveedor = "";
+            telefono_proveedor = "";
+            correo_proveedor = "";
+            domicilio_fiscal_proveedor = "";
+
+            nuevaConexion.Close();
+        }
+
+        private void ModificarDato()
+        {
+            nombre_proveedor = textNombre_Proveedor.Text;
+            telefono_proveedor = textTelefono_Proveedor.Text;
+            correo_proveedor = textCorreo_Proveedor.Text;
+            domicilio_fiscal_proveedor = textDomicilio_Proveedor.Text;
+
+            nuevaConexion.Open();
+
+            string insertInfo = "UPDATE almacen.Proveedor SET NombreProveedor = '" + nombre_proveedor + "', TelefonoProveedor = '" +
+                 telefono_proveedor + "', CorreoProveedor = '" + correo_proveedor + "', DomicilioFiscal = '" + domicilio_fiscal_proveedor + "' WHERE IdProveedor = '" + IdProveedor + "'";
+
+            SqlCommand cm = new SqlCommand(insertInfo, nuevaConexion);
+            cm.ExecuteNonQuery();
+
+            nuevaConexion.Close();
+
+
+        }
+
+        private void RemoverDato()
+        {
+           
+            nuevaConexion.Open();
+
+            string insertInfo = "DELETE FROM almacen.Proveedor WHERE IdProveedor = '" + IdProveedor + "'";
+
+            SqlCommand cm = new SqlCommand(insertInfo, nuevaConexion);
+            cm.ExecuteNonQuery();
+
+            nuevaConexion.Close();
+
+
+        }
+
+        private void Proveedor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonClickEliminar_Click(object sender, EventArgs e)
+        {
+            RemoverDato();
+            ConsultaDatos();
+            IdProveedor = "";
+            textNombre_Proveedor.Clear();
+            textTelefono_Proveedor.Clear();
+            textCorreo_Proveedor.Clear();
+            textDomicilio_Proveedor.Clear();
+        }
+
+        private void botonClickModificar_Click(object sender, EventArgs e)
+        {
+            ModificarDato();
+            ConsultaDatos();
+            IdProveedor = "";
+            textNombre_Proveedor.Clear();
+            textTelefono_Proveedor.Clear();
+            textCorreo_Proveedor.Clear();
+            textDomicilio_Proveedor.Clear();
+        }
+
+        private void botonClickInsertar_Click(object sender, EventArgs e)
+        {
+            InsertaDato();
+            ConsultaDatos();
+            textNombre_Proveedor.Clear();
+            textTelefono_Proveedor.Clear();
+            textCorreo_Proveedor.Clear();
+            textDomicilio_Proveedor.Clear();
+        }
+
+        private void TablaProveedor_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Obtén la fila seleccionada
+                DataGridViewRow filaSeleccionada = TablaProveedor.Rows[e.RowIndex];
+
+                // Obtén el valor de una celda específica (por ejemplo, la primera columna)
+                IdProveedor = filaSeleccionada.Cells[0].Value.ToString();
+                textNombre_Proveedor.Text = filaSeleccionada.Cells[1].Value.ToString();
+                textTelefono_Proveedor.Text = filaSeleccionada.Cells[2].Value.ToString();
+                textCorreo_Proveedor.Text = filaSeleccionada.Cells[3].Value.ToString();
+                textDomicilio_Proveedor.Text = filaSeleccionada.Cells[4].Value.ToString();
+
+            }
+        }
+    }
+}
